971eaaeccc990508720577760661c0746039d5a0
YoniKap <yonatan@projectcircle.co.il>
2025-10-23 10:10:05 +0300

    force upload

--- gh-oidc.tf
+++ gh-oidc.tf
@@ -1,3 +1,16 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+
+  client_id_list  = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+
+  tags = {
+    Name        = "GitHubActionsOIDC"
+    Environment = var.environment
+  }
+}
+
 resource "aws_iam_role" "github_oidc_role" {
   name               = "${var.environment}GitHubOidcRole"
   assume_role_policy = jsonencode({
